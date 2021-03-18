9be0fd80f3798c47dcd312cbc9328ecc3b2c9441
Spencer <sn.newton92@gmail.com>
2021-03-18 21:41:17 +0000

    Starter Script

---
+++ greeting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+user=$(whoami)
+day=$(date +%A)
+
+echo "Welcome back $user! Today is $day, which is the best day of the entire week!"
+echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
